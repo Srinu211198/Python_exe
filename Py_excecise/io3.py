98994d687e16b6940e3db378f2d74b25b2d90694
Srinu <sn20acy@gmail.com>
2023-10-21 14:49:34 +0100

    5 float inputs

--- Py_excecise/io3.py
+++ Py_excecise/io3.py
@@ -4,6 +4,10 @@ n = int(input("Enter the number:"))
 
 for i in range(1,n+1):
     if n % i == 0:
-        print(i, end= " ")
+        print(i, end=" ")
 
-#
+# accept a list of 5 float num as an input from user
+l = []
+for _ in range(5):
+    l.append(float(input("Enter your number:")))
+print(l)
