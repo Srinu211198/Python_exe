b71750175e1ee4022eb9b7f42c36070113ce13bd
Srinu <sn20acy@gmail.com>
2023-10-24 17:07:37 +0100

    loops

---
+++ Py_excecise/loops/sum_1st_10.py
@@ -0,0 +1,6 @@
+# print the sum of the current number and the previous number. WAP to iterate the first 10 numbers and in each iteration. print the sum of the current and previous number
+
+for i in range(1,11):
+    print(i+(i-1), end=" ")
+
+# WAP to search a given program from list
