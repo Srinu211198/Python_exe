b0560a32a053ed5afb3abe4ef55a757baec8d9ee
Srinu211198 <94193956+Srinu211198@users.noreply.github.com>
2024-05-01 13:39:26 +0100

    Add files via upload
    Basic Python file with basic operators

---
+++ Basic.py
@@ -0,0 +1,22 @@
+# intro
+
+print("Hello World!")
+
+print(2+3) #Addition
+print(3-2) #Substraction
+print(2*3) #Multiplication
+print(3/2) #Division
+print(2%3) #Modulus
+print(3//2) #Floor Division
+print(2**3) #exponential
+
+# Checking Data Types
+
+print(type(10))  #int
+print(type(10.0)) #float
+print(type(1+2j)) #Complex
+print(type('Hello Python')) #String
+print(type([1,2,3]))  #List
+print(type({'Name':'Python'})) #Dictionary
+print(type({9.8,3.14,2.7})) #set
+print(type((9.8,3.14,2.7))) #Tuple
