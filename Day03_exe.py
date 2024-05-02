369c3328c829398ce1df970d007fa909345c56d3
Srinu211198 <94193956+Srinu211198@users.noreply.github.com>
2024-05-02 09:51:06 +0100

    Add files via upload
    Day2 and Day 3 (50%)

---
+++ Day03_exe.py
@@ -0,0 +1,46 @@
+# Declare your age as integer variable
+# Declare your height as a float variable
+# Declare a variable that store a complex number
+age = 25;
+height = 5.8;
+c = 3+4j;
+
+# Write a script that prompts the user to enter base and height of the triangle and calculate an area of this triangle (area = 0.5 x b x h).
+base = int(input("Enter your base : "))
+height = float(input("Enter your height : "))
+print("Area of triangle: ", (base*height)/2)
+
+# Write a script that prompts the user to enter side a, side b, and side c of the triangle. Calculate the perimeter of the triangle (perimeter = a + b + c).
+a = int(input("Enter your 1st side : "))
+b = int(input("Enter your 2nd side : "))
+c = int(input("Enter your 3rd side : "))
+print("the perimeter of the triangle is :",a+b+c)
+
+# Get length and width of a rectangle using prompt. Calculate its area (area = length x width) and perimeter (perimeter = 2 x (length + width))
+length = int(input("Enter your length:"))
+width = int(input("Enter your width:"))
+print("Area is ",length*width)
+print("Parameter is ",2*(length*width))
+
+# Get radius of a circle using prompt. Calculate the area (area = pi x r x r) and circumference (c = 2 x pi x r) where pi = 3.14.
+r = int(input("Enter radius:"))
+pi = 3.14
+print("Area is ",pi*(r**2))
+
+
+# Calculate the slope, x-intercept and y-intercept of y = 2x -2
+x = int(input("Enter x-intercept:"))
+# y = int(input("Enter y-intercept:"))
+y = 2*x-2;
+x1,y1=2,2
+x2,y2=6,10
+
+# Calculate the slope, x-intercept and y-intercept of y = 2x -2
+# Slope is (m = y2-y1/x2-x1). Find the slope and Euclidean distance between point (2, 2) and point (6,10)
+# Compare the slopes in tasks 8 and 9.
+# Calculate the value of y (y = x^2 + 6x + 9). Try to use different x values and figure out at what x value y is going to be 0.
+
+# Find the length of 'python' and 'dragon' and make a falsy comparison statement.
+print("len(python) compare len(dragon):",len('Python')!=len('dragon'))
+
+
---
+++ Variables.py
@@ -0,0 +1,28 @@
+# Variables in python
+
+first_name = "Harry"
+last_name = "Potter"
+age = 21    
+Country = 'England'
+city = 'London'
+Capital_city= "London"
+is_marries = True
+hobbies = ('rides', 'pets','someother things')
+
+skills = ['Magic Spells','Can talk with snakes','Cursed',hobbies]
+Contacts = {'Caretaker1' : 'Hagrid',
+            'Caretaker2' : 'Dumble Door',
+            'secret_caretaker' : hobbies}
+
+friends = {('ron','Harmony'),('malfboy','wesley')}
+
+
+
+
+print(type(skills))
+
+print(type(Contacts))
+
+print(type(friends))
+
+print(type(hobbies))
